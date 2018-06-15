875fbe899453758772562d320208ecc25fae1989
Dmitry <crismsfiva@gmail.com>
2018-06-15 22:28:17 +0300

    Добавлено модальное окно
    1) Верстка модального окна + адаптив
    2) Реализован скрипт, по открытию/закрытию

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,52 @@
-var navMain = document.querySelector(".main-nav");
-var navToggle = document.querySelector(".main-nav__toggle");
+(function () {
+  var navMain = document.querySelector(".main-nav");
+  var navToggle = document.querySelector(".main-nav__toggle");
+  var overlay = document.querySelector(".overlay");
+  var modal = document.querySelector(".modal");
+  var modalBtn = document.querySelector(".offer__button");
 
-navMain.classList.remove("main-nav--nojs");
+  // Мобильное меню
+  navMain.classList.remove("main-nav--nojs");
 
-navToggle.addEventListener("click", function () {
-  if (navMain.classList.contains("main-nav--closed")) {
-    navMain.classList.remove("main-nav--closed");
-    navMain.classList.add("main-nav--opened");
-  } else {
-    navMain.classList.add("main-nav--closed");
-    navMain.classList.remove("main-nav--opened");
-  }
-});
+  navToggle.addEventListener("click", function () {
+    if (navMain.classList.contains("main-nav--closed")) {
+      navMain.classList.remove("main-nav--closed");
+      navMain.classList.add("main-nav--opened");
+    } else {
+      navMain.classList.add("main-nav--closed");
+      navMain.classList.remove("main-nav--opened");
+    }
+  });
+
+
+  // Модальное окно
+  modalBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    modal.classList.add("modal--show");
+    overlay.classList.add("overlay--show");
+  });
+
+  // Закрытие
+  overlay.addEventListener("click", function () {
+    if (modal.classList.contains("modal--show")) {
+      modal.classList.remove("modal--show");
+    }
+
+    if (overlay.classList.contains("overlay--show")) {
+      overlay.classList.remove("overlay--show");
+    }
+  });
+
+  window.addEventListener("keydown", function (e) {
+    if (e.keyCode === 27) {
+      modal.classList.remove("modal--show");
+      overlay.classList.remove("overlay--show");
+    }
+  })
+
+  modal.addEventListener("click", function (e) {
+    e.stopPropagation(); // для предотвращения закрытия модального окна при клике по нему. Источник: https://learn.javascript.ru/event-bubbling#прекращение-всплытия
+  });
+
+})();
